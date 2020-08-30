5422741006fa64c23b77bc6e41b2836c158b47b8
Mihir_Prajapati <mihir1737@gmail.com>
2020-08-30 11:10:17 +0530

    commit x

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from "./components/navbar.component"
 import ExercisesList from "./components/exercises-list.component";
 import EditExercise from "./components/edit-exercise.component";
 import CreateExercise from "./components/create-exercise.component";
-import CreateUser from "./components/create-user.component";
+import CreateUser from "./components/create-user.component"; 
 
 function App() {
   return (
