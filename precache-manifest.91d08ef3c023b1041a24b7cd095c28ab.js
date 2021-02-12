b901f21a74b4cda9aa69f9291433d4f8f7b788ba
unknown <mihir1737@gmail.com>
2021-02-13 00:03:56 +0530

    Updates

---
+++ precache-manifest.91d08ef3c023b1041a24b7cd095c28ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51f905a804c7500265232a2cc7fe241",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "6719089dd7bad2b76a65",
+    "url": "/my-app/static/css/2.630bca2d.chunk.css"
+  },
+  {
+    "revision": "6719089dd7bad2b76a65",
+    "url": "/my-app/static/js/2.612d2fa3.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/my-app/static/js/2.612d2fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9683138d4275595c51c1",
+    "url": "/my-app/static/js/main.dbb602f0.chunk.js"
+  },
+  {
+    "revision": "482269dda80b87d24fe7",
+    "url": "/my-app/static/js/runtime-main.4ffbee86.js"
+  }
+]);
